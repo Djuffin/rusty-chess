fcd87c3be7c0887a1b7f96c6b503f8df019bd0be
Eugene Zemtsov <e.zemtsov@gmail.com>
2015-05-29 02:49:52 -0700

    improve test speed in cargo build

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-rustc -C target-feature=sse3,sse4.1,sse4.2 -C opt-level=3 -g --cfg ndebug -o tests_rchess  --test main.rs 
+rustc -L ./target/debug/deps/ --extern rand=./target/debug/deps/librand-b924d9fc5b3eb5b8.rlib -C target-feature=sse3,sse4.1,sse4.2 -C opt-level=3 -g --cfg ndebug -o tests_rchess  --test src/main.rs 
